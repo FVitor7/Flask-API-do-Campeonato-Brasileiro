9bc9538df6d800950ac068de51f7659f57c9326a
Fábio Vitor Coelho Silva <48036134+FVitor7@users.noreply.github.com>
2020-10-07 02:20:02 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -102,4 +102,7 @@ class ApiGet(Resource):
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = float(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
